ad960e25f6d84a6d981537c5e6ee8976b6e3db50
PiYuSh7-2 <pk7427120@gmail.com>
2024-11-13 23:53:54 +0530

    initial commit

---
+++ schema.sql
@@ -0,0 +1,10 @@
+create database onlineapp;
+use onlineapp;
+create table users
+(
+ id integer auto_increment primary key,
+ uname varchar(100) not null,
+ email varchar(50) not null unique,
+ country varchar(50) not null,
+ passwd varchar(20) not null
+);
