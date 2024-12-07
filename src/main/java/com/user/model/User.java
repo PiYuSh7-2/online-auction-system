ff5c9224ad76f4d6256e4195f6c67fa1186a66d0
PiYuSh7-2 <pk7427120@gmail.com>
2024-12-07 10:34:45 +0530

    updated file

--- src/main/java/com/user/model/User.java
+++ src/main/java/com/user/model/User.java
@@ -1,58 +1,37 @@
 package com.user.model;
 
 public class User {
-	private int id;
-	private String name;
-	private String email;
-	private String country;
-	private String password;
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public User(int id, String name, String email, String country, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.country = country;
-		this.password = password;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", country=" + country + ", password="
-				+ password + "]";
-	}
-	
-	
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {}
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
+
+
