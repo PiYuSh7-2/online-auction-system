88e7c31a2550856979376d5c6959409485f1aaa9
Piyush Kumar <144892788+PiYuSh7-2@users.noreply.github.com>
2024-12-07 11:22:51 +0530

    Update UserDAO.java

--- src/main/java/com/user/dao/UserDAO.java
+++ src/main/java/com/user/dao/UserDAO.java
@@ -1,156 +1,40 @@
 package com.user.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.user.model.User;
+import java.sql.*;
 
 public class UserDAO {
-	private String jdbcURL="jdbc:mysql://localhost:30006/onlineapp";
-	private String jdbcUserName="root";
-	private String jdbcPassword="root";
-	
-	
-	private static final String INSERT_USER_SQL="INSERT INTO users"+"(uname,email,country,passwd) VALUES"+"(?,?,?,?);";
-	private static final String SELECT_USER_BY_ID="SELECT *FROM USERS WHERE ID=?;";
-	private static final String SELECT_ALL_USERS="SELECT *FROM USERS;";
-	private static final String DELETE_USERS_SQL="DELETE FROM USERS WHERE ID=?;";
-	private static final String UPDATE_USERS_SQL="UPDATE USERS SET UNAME=?, EMAIL=?, COUNTRY=?, PASSWORD=? WHERE ID=?;";
-	
-	
-	public UserDAO() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Connection getConnection() {
-        Connection connection = null;
+    private final String url = "jdbc:mysql://localhost:3306/online_auction";
+    private final String username = "root";
+    private final String password = "9142480211";
 
-        try 
-        {
-        	Class.forName("com.mysql.cj.jdbc.Driver");
-        	connection=DriverManager.getConnection(jdbcURL, jdbcUserName, jdbcPassword);
-
-        	
+    public boolean registerUser(User user) {
+        String query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getEmail());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch(SQLException | ClassNotFoundException e)
-        {
-        	e.printStackTrace();
-        }
-        catch(Exception e)
-        {
-        	e.printStackTrace();
-        }
-        return connection;
-	}
-	
-	
-	public void insertUser(User user)
-	{
+        return false;
+    }
 
-		try(Connection connection=dao.getConnection())
-		{
-			PreparedStatement preparedStatement=connection.prepareStatement(INSERT_USER_SQL);
-			preparedStatement.setString(1, user.getName());
-			preparedStatement.setString(2,user.getEmail());
-			preparedStatement.setString(3,user.getCountry());
-			preparedStatement.setString(4,user.getPassword());
-			
-			preparedStatement.executeUpdate();		
-		}
-		catch(Exception e)
-        {
-        	e.printStackTrace();
+    public User validateUser(String username, String password) {
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try (Connection conn = DriverManager.getConnection(url, this.username, this.password);
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getInt("id"), rs.getString("username"), rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-	}
-	
-	public User selectUser(int id)
-	{
-		User user=new User();
-		UserDAO dao = new UserDAO();
-
-		try(Connection connection=dao.getConnection())
-		{
-			PreparedStatement preparedStatement=connection.prepareStatement(SELECT_USER_BY_ID);
-			preparedStatement.setInt(1, id);
-			
-			ResultSet resultSet=preparedStatement.executeQuery();
-			
-			while(resultSet.next())
-			{
-				user.setId(id);
-				user.setName(resultSet.getString("uname"));
-				user.setEmail(resultSet.getString("email"));
-				user.setCountry(resultSet.getString("country"));
-				user.setPassword(resultSet.getString("passwd"));
-			}
-				
-		}
-		catch(Exception e)
-	    {
-	        e.printStackTrace();
-	    }
-		return user;
-		}
-	
-	
-	
-	public List<User> selectAllUsers()
-	{
-		List<User> users=new ArrayList<User>();
-		UserDAO dao = new UserDAO();
-		
-		try(Connection connection=dao.getConnection())
-		{
-			PreparedStatement preparedStatement=connection.prepareStatement(SELECT_ALL_USERS);
-			ResultSet resultSet=preparedStatement.executeQuery();
-			
-			
-			
-			while(resultSet.next())
-			{
-				int id=resultSet.getInt("id");
-				String uname=resultSet.getString("uname");
-				String email=resultSet.getString("email");
-				String country=resultSet.getString("country");
-				String password=resultSet.getString("passwd");
-				
-				
-				users.add(new User(id,uname,email,country,password));
-			}
-			
-				
-		}
-		catch(Exception e)
-	    {
-	        e.printStackTrace();
-	    }
-		return users;	
-		}
-	public boolean deleteUser(int id)
-	{
-		boolean status=false;
-		UserDAO dao = new UserDAO();
-		try(Connection connection=dao.getConnection())
-		{
-			PreparedStatement preparedStatement=connection.prepareStatement(DELETE_USERS_SQL);
-			preparedStatement.setInt(1, id);
-			
-			
-			status=preparedStatement.execute();
-		}
-		catch(Exception e)
-	    {
-	        e.printStackTrace();
-	    }
-		return status;
-		}
-	}
-	
-
+        return null;
+    }
 }
